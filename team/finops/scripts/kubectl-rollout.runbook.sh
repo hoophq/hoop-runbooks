79cc0fea309188a82cbc25a683ef3ec0f41413e4
San <sandromll@gmail.com>
2023-03-13 11:18:46 -0300

    team/finops/scripts/kubectl-rollout.runbook.sh

--- team/finops/scripts/kubectl-rollout.runbook.sh
+++ team/finops/scripts/kubectl-rollout.runbook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 : '
-{{ .namespace | required "namespace is required" | type "text" | asenv "NAMESPACE" }}
+{{ .namespace | required "namespace is required" | type "select" | options "dev" "prod" | asenv "NAMESPACE" }}
 {{ .deploy_name | required "deploy name is required" | type "text" | asenv "DEPLOYMENT" }}
 '
 exec kubectl --namespace $NAMESPACE rollout restart deploy/$DEPLOYMENT
